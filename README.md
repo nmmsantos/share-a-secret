4d2bcaf7626f2c1f67db256611128bde6e258865
Nuno Santos <eu.nuno@gmail.com>
2018-04-22 01:13:55 +0100

    Added compression

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # share-a-secret
 Store secrets securely on your browser's bookmarks
+
+Visit at https://c1rcu17.github.io/share-a-secret
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,17 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
   lintOnSave: false,
   configureWebpack: () => {
     if (process.env.NODE_ENV === 'production') {
       return {
-        devtool: 'none'
+        devtool: 'none',
+        plugins: [
+          new CompressionPlugin({
+            test: /\.(js|css)$/
+          })
+        ]
       };
     }
     return {};
